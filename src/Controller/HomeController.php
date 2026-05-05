bec804f1543d2b4ac4880a87fd6853dd8374b53b
Yannick Lepinay <lepinayyannick7@gmail.com>
2026-05-05 14:16:36 +0200

    feat: carte interactive globale avec lieux terrains et evenements

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\EvenementRepository;
 use App\Repository\LieuRepository;
 use App\Repository\TerrainRepository;
-use App\Repository\EvenementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -18,9 +18,26 @@ class HomeController extends AbstractController
         EvenementRepository $evenementRepo
     ): Response {
         return $this->render('home/index.html.twig', [
-            'derniers_lieux'        => $lieuRepo->findBy(['estValide' => true], ['createdAt' => 'DESC'], 6),
-            'derniers_terrains'     => $terrainRepo->findBy(['estDisponible' => true], ['createdAt' => 'DESC'], 3),
-            'prochains_evenements'  => $evenementRepo->findBy([], ['dateDebut' => 'ASC'], 3),
+            'derniers_lieux'       => $lieuRepo->findBy(['estValide' => true], ['createdAt' => 'DESC'], 6),
+            'derniers_terrains'    => $terrainRepo->findBy(['estDisponible' => true], ['createdAt' => 'DESC'], 3),
+            'prochains_evenements' => $evenementRepo->findBy([], ['dateDebut' => 'ASC'], 3),
+        ]);
+    }
+
+    #[Route('/carte', name: 'app_carte')]
+    public function carte(
+        LieuRepository $lieuRepo,
+        TerrainRepository $terrainRepo,
+        EvenementRepository $evenementRepo
+    ): Response {
+        $lieux     = $lieuRepo->findBy(['estValide' => true]);
+        $terrains  = $terrainRepo->findBy(['estDisponible' => true]);
+        $evenements = $evenementRepo->findByFiltres();
+
+        return $this->render('home/carte.html.twig', [
+            'lieux'      => $lieux,
+            'terrains'   => $terrains,
+            'evenements' => $evenements,
         ]);
     }
 }
