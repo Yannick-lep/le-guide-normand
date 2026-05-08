44bf6714e4cb810dc9d97b10f5d66bf60616e85f
Yannick Lepinay <lepinayyannick7@gmail.com>
2026-05-08 13:16:12 +0200

    feat: stats reelles sur la page d'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,24 +5,32 @@ namespace App\Controller;
 use App\Repository\EvenementRepository;
 use App\Repository\LieuRepository;
 use App\Repository\TerrainRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(
-        LieuRepository $lieuRepo,
-        TerrainRepository $terrainRepo,
-        EvenementRepository $evenementRepo
-    ): Response {
-        return $this->render('home/index.html.twig', [
-            'derniers_lieux'       => $lieuRepo->findBy(['estValide' => true], ['createdAt' => 'DESC'], 6),
-            'derniers_terrains'    => $terrainRepo->findBy(['estDisponible' => true], ['createdAt' => 'DESC'], 3),
-            'prochains_evenements' => $evenementRepo->findBy([], ['dateDebut' => 'ASC'], 3),
-        ]);
-    }
+   #[Route('/', name: 'app_home')]
+public function index(
+    LieuRepository $lieuRepo,
+    TerrainRepository $terrainRepo,
+    EvenementRepository $evenementRepo,
+    UserRepository $userRepo
+): Response {
+    return $this->render('home/index.html.twig', [
+        'derniers_lieux'       => $lieuRepo->findBy(['estValide' => true], ['createdAt' => 'DESC'], 6),
+        'derniers_terrains'    => $terrainRepo->findBy(['estDisponible' => true], ['createdAt' => 'DESC'], 3),
+        'prochains_evenements' => $evenementRepo->findByFiltres(null, null),
+        'stats' => [
+            'lieux'      => $lieuRepo->count(['estValide' => true]),
+            'terrains'   => $terrainRepo->count(['estDisponible' => true]),
+            'evenements' => $evenementRepo->count([]),
+            'membres'    => $userRepo->count([]),
+        ],
+    ]);
+}
 
     #[Route('/carte', name: 'app_carte')]
     public function carte(
